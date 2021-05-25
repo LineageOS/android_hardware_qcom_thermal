d1c4fa05ae8e468b9c51e77f23d06b02071f70be
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2021-05-25 22:20:39 +0530

    thermal-hal: Add thermal HAL 2.0 support for blair
    Add thermal HAL 2.0 support for blair target.
    It shares sensor configuration with holi.
    
    Change-Id: I1ae5e027b23f0391a7102ba3292366b456033a09

--- thermalConfig.cpp
+++ thermalConfig.cpp
@@ -1775,6 +1775,7 @@ namespace implementation {
 		{502, lahaina_common},
 		{450, lahaina_common}, // shima
 		{454, sensor_cfg_holi}, // holi
+		{507, sensor_cfg_holi}, // blair
 		{475, sensor_cfg_yupik}, // yupik
 		{515, sensor_cfg_yupik}, // YUPIK-LTE
 		{457, waipio_common}, //Waipio
