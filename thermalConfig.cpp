2040479944dedc31cbb47fe21c8407e36f07572d
Nitesh Kumar <quic_nitekuma@quicinc.com>
2024-04-11 13:03:33 +0530

    thermal-hal: Add Netrani pro SoC ID
    Add Netrani pro SoC ID.
    
    Signed-off-by: Nitesh Kumar <quic_nitekuma@quicinc.com>
    Change-Id: I1b498959ee29a4fe55570d071cea0da47485bfd6

--- thermalConfig.cpp
+++ thermalConfig.cpp
@@ -1325,6 +1325,7 @@ namespace implementation {
 		{633, parrot_common}, //Netrani Gaming SKU with modem
 		{634, parrot_common}, //Netrani Gaming SKU without modem
 		{638, parrot_common}, //SM7435P
+		{663, parrot_common}, //SM7435
 		{549, anorak_common},
 		{649, anorak_common}, // Halliday Pro
 		{568, ravelin_common}, //Clarence Mobile
@@ -1357,6 +1358,7 @@ namespace implementation {
 		{583, parrot_specific}, //Netrani mobile without modem
 		{613, parrot_specific}, //Netrani APQ
 		{631, parrot_specific},
+		{663, parrot_specific}, //Netrani pro
 		{549, anorak_specific},
 		{649, anorak_specific}, // Halliday Pro
 		{591, waipio_specific}, //ukee
