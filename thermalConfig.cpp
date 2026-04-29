f969d38df5c1a8cb895df5bb6de5097e7129a3da
Ajay Kumar Reddy Kathi <akathi@qti.qualcomm.com>
2026-04-29 16:52:08 +0530

    thermal-hal: Add thermal HAL support for bourtzi
    Add thermal HAL support for bourtzi.
    
    Change-Id: Ibb4ebe60d989f95a9dc2832c206d9f2358acac3b
    Signed-off-by: Ajay Kumar Reddy Kathi <akathi@qti.qualcomm.com>

--- thermalConfig.cpp
+++ thermalConfig.cpp
@@ -2988,6 +2988,92 @@ namespace thermal {
 	};
 
 
+	std::vector<std::string> cpu_sensors_bourtzi = {
+		"cpu-0-0",
+		"cpu-0-1",
+		"cpu-0-2",
+		"cpu-0-3",
+		"cpu-0-4",
+		"cpu-0-5",
+		"cpu-1-0",
+		"cpu-1-2",
+	};
+	std::vector<struct target_therm_cfg> bourtzi_common = {
+		{
+			TemperatureType::CPU,
+			cpu_sensors_bourtzi,
+			"",
+			{
+			[SEVERE] = 95000,
+			[SHUTDOWN] = 115000,
+			},
+			true,
+			true,
+		},
+		{
+			TemperatureType::GPU,
+			{ "gpuss-0" },
+			"GPU0",
+			{
+			[SEVERE] = 95000,
+			[SHUTDOWN] = 115000,
+			},
+			true,
+			true,
+		},
+		{
+			TemperatureType::GPU,
+			{ "gpuss-1" },
+			"GPU1",
+			{
+			[SEVERE] = 95000,
+			[SHUTDOWN] = 115000,
+			},
+			true,
+			true,
+		},
+	};
+
+	std::vector<struct target_therm_cfg>  bourtzi_specific = {
+		{
+			TemperatureType::SKIN,
+			{ "sys-therm-0" },
+			"skin",
+			{
+			[LIGHT] = 48000,
+			[MODERATE] = 49000,
+			[SEVERE] = 50000,
+			[CRITICAL] = 60000,
+			[EMERGENCY] = 61000,
+			[SHUTDOWN] = 90000,
+			},
+			true,
+			true,
+		},
+		{
+			TemperatureType::BCL_CURRENT,
+			{ "pm7250b-ibat-lvl0" },
+			"ibat",
+			{
+			[SEVERE] = 6000,
+			[SHUTDOWN] = 7500,
+			},
+			true,
+			true,
+		},
+		{
+			TemperatureType::BCL_CURRENT,
+			{ "pmiv010x-ibat-lvl0" },
+			"ibat",
+			{
+			[SEVERE] = 6000,
+			[SHUTDOWN] = 7500,
+			},
+			true,
+			true,
+		},
+	};
+
 	const std::unordered_map<int, std::vector<struct target_therm_cfg>>
 		msm_soc_map = {
 		{417, sensor_cfg_bengal}, // bengal
@@ -3066,6 +3152,8 @@ namespace thermal {
 		{744, chora_common}, //APQ Skyros
 		{733, malabar_common}, //Aldabara
 		{757, malabar_common}, //APQ Aldabara
+		{776, bourtzi_common}, //Poros-SM4875
+		{777, bourtzi_common}, //Poros-SM4875P
 	};
 
 	const std::unordered_map<int, std::vector<struct target_therm_cfg>>
@@ -3110,6 +3198,8 @@ namespace thermal {
 		{744, chora_specific}, //APQ Skyros
 		{733, malabar_specific}, // Aldabra
 		{757, malabar_specific}, //APQ Aldabra
+		{776, bourtzi_specific}, //Poros-SM4875
+		{777, bourtzi_specific}, //Poros-SM4875P
 	};
 
 	const std::unordered_multimap<int, std::pair<int, std::vector<struct target_therm_cfg>>>
